d5b91cc08c4f8ccb9645c435abdb6289aa197762
Santiago Moreno <smoreno@psicol.co>
2019-08-24 11:28:10 -0500

    feature/passport Entrega de tokens funcionando al momento de registrarse

--- app/User.php
+++ app/User.php
@@ -5,11 +5,12 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Passport\Passport;
 use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable, HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
